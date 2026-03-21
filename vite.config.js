385db8362cfe7071d026c1862295180d38fb8d96
Michael Red'kov <michael1redkov@gmail.com>
2026-03-21 16:32:26 +0300

    Fix: set base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig((mode) => {
-  const isProd = mode === 'production'
+export default defineConfig(({ mode }) => {
+    const isProd = mode === 'production'
 
-  return {
-      base: isProd ? '/todo-react/' : '/',
-      plugins: [react()],
-      resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+    return {
+        base: isProd ? '/todo-react/' : '/',
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        }
     }
-  }
 })
